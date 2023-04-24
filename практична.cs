2d2b81b83dbda5ad2992e80c0edefe8a8baba38a
Parnakmuradkhanyan <parnak725@gmail.com>
2023-04-24 19:34:34 +0300

    Initial commit

---
+++ практична.cs
@@ -0,0 +1,18 @@
+using System.Text;
+using System;
+using System.Threading;
+
+namespace Labarotory_work
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Student: Muradkhanyan P.");
+            Console.WriteLine("Faculty: Faculty of Information Technology");
+            Console.WriteLine("Speciality: Software Engineering");
+            Console.WriteLine("Form of education: Daytime");
+            Console.WriteLine("Group: 1-5");
+        }
+    }
+}
